2497b67519453499a056d22d38a892642639bf3f
Namukk <skadnr625@gmail.com>
2021-05-03 17:19:19 +0900

    Algorithm Start

---
+++ python/test.py
@@ -0,0 +1,17 @@
+# a = list(input())
+# b = set(a)
+# c = list(b)
+# c.sort()
+
+# import sys
+# n = int(input())
+# l = []
+# for i in range(n):
+#     l.append(int(sys.stdin.readline()))
+# for i in sorted(l):
+#     sys.stdout.write(str(i)+'\n')
+
+
+
+
+
