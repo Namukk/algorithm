c9aa630258f1151fd19ad348896ae46012149ec5
Namukk <skadnr625@gmail.com>
2021-05-03 18:04:02 +0900

    A+B -3

--- python/BOJ/10950.py
+++ python/BOJ/10950.py
@@ -1,15 +1,15 @@
-T = int(input())
-
-nums=[]
-
-for i in range(T):
-    [a,b] = map(int,input().split())
-    nums.append([a,b])
-    print(nums[i][0] + nums[i][1])
-
-
 # T = int(input())
 
+# nums=[]
+
 # for i in range(T):
-#     a,b = map(int,input().split())
-#     print(a+b)
+#     [a,b] = map(int,input().split())
+#     nums.append([a,b])
+#     print(nums[i][0] + nums[i][1])
+
+
+T = int(input())
+
+for i in range(T):
+    a,b = map(int,input().split())
+    print(a+b)
