ea4ce87053b04539e21ed055442912be4f120c34
Namukk <skadnr625@gmail.com>
2021-06-17 02:09:55 +0300

    별 찍기 -16

---
+++ python/BOJ/10991.py
@@ -0,0 +1,9 @@
+N = int(input())
+
+for i in range(1, N+1):
+    print(" "*(N-i) + "* "*(i-1) + "*")
+
+
+# https://jyeonnyang2.tistory.com/33 참고
+
+#https://jinyes-tistory.tistory.com/63 ???????? 확인
