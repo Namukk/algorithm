f8567f1ab8ff623fb9e1f9b1b4d716a133bd962c
Namukk <skadnr625@gmail.com>
2021-06-21 23:40:45 +0300

    수 정렬하기 -2

---
+++ python/BOJ/2751.py
@@ -0,0 +1,25 @@
+N=int(input())
+L=[]
+
+for i in range(N):
+    a=int(input())
+    L.append(a)
+
+sorted_L = sorted(L)
+
+for j in sorted_L:
+    print(j)
+    
+
+
+# 다른 내 정답
+# N = int(input())
+# num = []
+
+# for i in range(N):
+#     num.append(int(input()))
+
+# num = sorted(set(num))
+
+# for i in num:
+#     print(i)
