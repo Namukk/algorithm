3aca4490533e28a67adba40b01d0a86493cd1190
Namukk <skadnr625@gmail.com>
2021-07-01 06:14:51 +0900

    2진수 8진수

---
+++ python/BOJ/1373.py
@@ -0,0 +1,7 @@
+print(oct(int(input(), 2))[2:])
+
+# https://velog.io/@kwkim95/210331%EB%B0%B1%EC%A4%80BOJ-1373%EB%B2%88-2%EC%A7%84%EC%88%98-8%EC%A7%84%EC%88%98    답 참고
+
+
+# https://oort-cloud.tistory.com/53
+#https://duwjdtn11.tistory.com/512     두개 참고
