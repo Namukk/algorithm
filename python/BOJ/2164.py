639b9c279b431e040192a6f33c4cd8e5cb1b6c27
Namukk <skadnr625@gmail.com>
2021-07-27 17:22:11 +0900

    카드 -2

--- python/BOJ/2164.py
+++ python/BOJ/2164.py
@@ -10,3 +10,8 @@ while len(q) > 1:
     q.append(q.popleft())
 
 print(q.pop())
+
+
+# https://yunaaaas.tistory.com/31   답 참고
+# https://tooo1.tistory.com/88   이런 방식도(시간복잡도 참고)
+# https://suri78.tistory.com/50   처음 내 생각이랑 비슷 
